cbe39de046cc01f11cf9f53a5d46f28a03e3bebc
Akkarapon Phikulsri <lacakira@gmail.com>
2022-10-08 08:35:19 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,25 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { CashflowModule } from './cashflow/cashflow.module';
+import { PocketModule } from './pocket/pocket.module';
+import { TaxplanModule } from './taxplan/taxplan.module';
+import { BalancesheetModule } from './balancesheet/balancesheet.module';
+import { TransferModule } from './transfer/transfer.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), 
     DatabaseModule,
     AuthModule,
-    UserModule
+    UserModule,
+    CashflowModule,
+    PocketModule,
+    TaxplanModule,
+    BalancesheetModule,
+    TransferModule,
+    CategoriesModule
   ],
 })
 export class AppModule { }
