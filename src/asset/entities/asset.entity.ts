fc96f94c408f44396c78d60fb12b66db8b2cc8b4
Akkarapon Phikulsri <lacakira@gmail.com>
2022-10-10 14:08:25 +0700

    Update asset.entity.ts

--- src/asset/entities/asset.entity.ts
+++ src/asset/entities/asset.entity.ts
@@ -1,1 +1,57 @@
-export class Asset {}
+import { ApiProperty } from "@nestjs/swagger";
+import { UUIDV4 } from "sequelize";
+import { Column, DataType, Table, HasOne, Model, ForeignKey, BelongsTo, BelongsToMany, HasMany } from "sequelize-typescript";
+
+export enum AssetEnum {
+	LIQUID = 'liquid',
+	PRIVATE = 'private',
+	INVESTMENT = 'investment',
+	INTANGIBLE = 'intangible'
+}
+
+@Table({
+	tableName: 'asset'
+})
+export class AssetAttributes extends Model<AssetAttributes> {
+	@ApiProperty()
+	@Column({
+		type: DataType.UUID,
+		defaultValue: UUIDV4,
+		allowNull: false,
+		primaryKey: true,
+	})
+	id: string
+
+	@ApiProperty()
+	@Column({
+		type: DataType.DECIMAL(10, 2),
+	})
+	value: number;
+
+	@ApiProperty()
+	@Column({
+		type: DataType.DECIMAL(10, 2),
+		field: "cashflow_per_year"
+	})
+	cashflowPerYear: number;
+
+	@ApiProperty()
+	@Column({
+		type: DataType.ENUM({
+			values: [
+				AssetEnum.LIQUID,
+				AssetEnum.PRIVATE,
+				AssetEnum.INVESTMENT,
+				AssetEnum.INTANGIBLE
+			]
+		}),
+		defaultValue: AssetEnum.LIQUID, 
+		allowNull: false
+
+	})
+	type: AssetEnum;
+
+
+
+
+}
