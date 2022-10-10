d21e5144b7d75cd12a0b05608c9b75a87f9e2110
Akkarapon Phikulsri <lacakira@gmail.com>
2022-10-10 14:14:17 +0700

    hasMany --> (Transfer, Asset, Debt, Cashflowin, Cashflowout)

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { UUIDV4 } from "sequelize";
 import { Column, DataType, Table, HasOne, Model, ForeignKey, BelongsTo, BelongsToMany, HasMany } from "sequelize-typescript";
-import { CashflowAttributes } from "src/cashflow/entities/cashflow.entity";
+import { AssetAttributes } from "src/asset/entities/asset.entity";
+import { CashflowinAttributes } from "src/cashflowin/entities/cashflowin.entity";
+import { CashflowoutAttributes } from "src/cashflowout/entities/cashflowout.entity";
+import { DebtAttributes } from "src/debt/entities/debt.entity";
 import { PocketAttributes } from "src/pocket/entities/pocket.entity";
+import { TransferAttributes } from "src/transfer/entities/transfer.entity";
 import { Role } from "./role.enum";
 
 @Table({
@@ -70,7 +74,21 @@ export class UserAttributes extends Model<UserAttributes> {
 	@HasMany(() => PocketAttributes)
 	pockets: PocketAttributes[]
 
-	@HasMany(() => CashflowAttributes)
-	cashflows: CashflowAttributes[]
+	@HasMany(() => CashflowinAttributes)
+	cashflowins: CashflowinAttributes[]
+
+	@HasMany(() => CashflowoutAttributes)
+	cashflowouts: CashflowoutAttributes[]
+
+	@HasMany(() => AssetAttributes)
+	assets: AssetAttributes[]
+
+	@HasMany(() => DebtAttributes)
+	debts: DebtAttributes[]
+
+	@HasMany(() => TransferAttributes)
+	transfers: TransferAttributes[]
+
+	
 
 }
