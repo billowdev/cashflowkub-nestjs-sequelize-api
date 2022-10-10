77abf018e107b02c4131a98602397ef816bd3f9a
Akkarapon Phikulsri <lacakira@gmail.com>
2022-10-10 14:08:28 +0700

    Update cashflowin.entity.ts

--- src/cashflowin/entities/cashflowin.entity.ts
+++ src/cashflowin/entities/cashflowin.entity.ts
@@ -1,1 +1,30 @@
-export class Cashflowin {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { UUIDV4 } from "sequelize/types";
+
+@Table({
+	tableName: "cashflowin"
+})
+export class CashflowinAttributes extends Model<CashflowinAttributes> {
+	@ApiProperty()
+	@Column({
+		type: DataType.UUID,
+		defaultValue: UUIDV4,
+		allowNull: false,
+		primaryKey: true,
+	})
+	id: string
+
+	@ApiProperty()
+	@Column({
+		type: DataType.STRING(150),
+	})
+	desc: string;
+
+	@ApiProperty()
+	@Column({
+		type: DataType.DECIMAL(10, 2),
+	})
+	amount: number;
+
+}
