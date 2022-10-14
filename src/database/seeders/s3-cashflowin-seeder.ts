49aab6c155af806661ad3f61cb0dd63d0b2f5283
Akkarapon Phikulsri <lacakira@gmail.com>
2022-10-14 18:10:36 +0700

    Update s3-cashflowin-seeder.ts

--- src/database/seeders/s3-cashflowin-seeder.ts
+++ src/database/seeders/s3-cashflowin-seeder.ts
@@ -1,6 +1,8 @@
 'use strict';
-import { QueryInterface } from "sequelize";
+import { DataTypes, QueryInterface, UUIDV4 } from "sequelize";
 import { v4 as uuidv4 } from 'uuid';
+import { Sequelize } from 'sequelize-typescript'
+// const model = options.model; 
 
 type CashflowinType = {
 	id: string,
@@ -13,14 +15,14 @@ type CashflowinType = {
 	updated_at: Date,
 }
 
-
 module.exports = {
 	up: async (queryInterface: QueryInterface) => {
+
 		const cashflowinData: Array<CashflowinType> = [
 			{
 				id: uuidv4(),
 				desc: "income 1",
-				amount: 200,
+				amount: 5000,
 				user_id: "8731c5ce-2dcb-47da-8efd-ff0e07a6b050",
 				pocket_id: "1755fae2-ebf9-480c-849d-6ad23db0fdd0",
 				category_id: "3aa4a655-940e-46b1-9d4f-4ba90f22bfb2",
@@ -30,7 +32,7 @@ module.exports = {
 			{
 				id: uuidv4(),
 				desc: "income 2",
-				amount: 200,
+				amount: 1000,
 				user_id: "8731c5ce-2dcb-47da-8efd-ff0e07a6b050",
 				pocket_id: "1755fae2-ebf9-480c-849d-6ad23db0fdd0",
 				category_id: "3aa4a655-940e-46b1-9d4f-4ba90f22bfb2",
@@ -38,7 +40,25 @@ module.exports = {
 				updated_at: new Date(),
 			}
 		]
-		return queryInterface.bulkInsert('cashflowin', cashflowinData, {})
+		return Promise.all([
+			queryInterface.bulkInsert('cashflowin', cashflowinData, {}),
+
+			queryInterface.bulkUpdate('pocket',
+				{
+					balance: 5000
+				},
+				{ id: "1755fae2-ebf9-480c-849d-6ad23db0fdd0" }
+			),
+
+			queryInterface.bulkUpdate('pocket',
+				{
+					balance: 1000
+				},
+				{ id: "1755fae2-ebf9-480c-849d-6ad23db0fdd1" }
+			)
+
+
+		])
 	},
 
 	down: async (queryInterface: QueryInterface) => {
