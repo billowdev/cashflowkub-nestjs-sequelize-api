1f6aa8bb09054afd46c3cce4884662cb3efecb49
Akkarapon Phikulsri <lacakira@gmail.com>
2022-10-15 15:33:04 +0700

    Update transaction.module.ts

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
+import { transactionProviders } from './entities/transaction.providers';
 
 @Module({
   controllers: [TransactionController],
-  providers: [TransactionService]
+  providers: [TransactionService, ...transactionProviders]
 })
 export class TransactionModule {}
