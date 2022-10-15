b93db54a7bf379a8bc108d57f68563a944fef026
Akkarapon Phikulsri <lacakira@gmail.com>
2022-10-15 14:39:17 +0700

    Update pocket.service.ts

--- src/pocket/pocket.service.ts
+++ src/pocket/pocket.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { POCKET_REPOSITORY } from 'src/core/constants';
 import { CreatePocketDto } from './dto/create-pocket.dto';
 import { UpdatePocketDto } from './dto/update-pocket.dto';
@@ -8,25 +8,51 @@ import { PocketEntity } from './entities/pocket.entity';
 export class PocketService {
   constructor(
     @Inject(POCKET_REPOSITORY) private readonly pocketRepo: typeof PocketEntity
-  ){}
+  ) { }
 
-  create(createPocketDto: CreatePocketDto) {
-    return 'This action adds a new pocket';
+  async create(createPocketDto: CreatePocketDto): Promise<PocketEntity> {
+    try {
+      return await this.pocketRepo.create<PocketEntity>(createPocketDto)
+    } catch (error) {
+      throw new BadRequestException('create pocket failed')
+    }
   }
 
-  findAll() {
-    return `This action returns all pocket`;
+  async findAll(userId: string) {
+    try {
+      return await this.pocketRepo.findAll({
+        where: { userId }
+      })
+    } catch (error) {
+      throw new BadRequestException('get all pocket failed')
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pocket`;
+  async findOne(id: string, userId: string) {
+    try {
+      return await this.pocketRepo.findOne({
+        where: { id, userId }
+      })
+    } catch (error) {
+      throw new BadRequestException('get pocket by id failed')
+    }
   }
 
-  update(id: number, updatePocketDto: UpdatePocketDto) {
-    return `This action updates a #${id} pocket`;
+  async update(id: string, updatePocketDto: UpdatePocketDto, userId: string) {
+    try {
+      return await this.pocketRepo.update({ ...updatePocketDto }, { where: { id, userId } })
+    } catch (error) {
+      throw new BadRequestException('update pocket failed')
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pocket`;
+  async remove(id: string, userId: string) {
+    try {
+      return await this.pocketRepo.destroy({
+        where: { id, userId }
+      })
+    } catch (error) {
+      throw new BadRequestException('delete pocket failed')
+    }
   }
 }
