371e7dced0f1d029131ea88c3f7a6518784644ee
Akkarapon Phikulsri <lacakira@gmail.com>
2022-10-15 16:01:20 +0700

    Update transaction.controller.ts

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,38 +1,67 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Res } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 
 import { ApiTags } from '@nestjs/swagger';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { requestAuthUserDto } from 'src/auth/dto';
+import { FastifyReply } from 'fastify';
 
 
 @ApiTags('transactions')
 @Controller('transactions')
 export class TransactionController {
-  constructor(private readonly transactionService: TransactionService) {}
+  constructor(private readonly transactionService: TransactionService) { }
 
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionService.create(createTransactionDto);
+  async create(@Body() createTransactionDto: CreateTransactionDto,
+    @Res() res: FastifyReply) {
+    const data = await this.transactionService.create(createTransactionDto);
+    if (data) {
+      res.status(200).send({
+        statusCode: res.statusCode,
+        message: "create transaction successfuly",
+        data
+      })
+    } else {
+      res.status(400).send({
+        statusCode: res.statusCode,
+        message: "create transaction failed",
+        data: {}
+      })
+    }
   }
 
   @Get()
-  findAll() {
-    return this.transactionService.findAll();
+  findAll(
+    @Req() req: requestAuthUserDto,
+    @Res() res: FastifyReply
+  ) {
+    const userId = req.user.sub
+    return this.transactionService.findAll(userId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionService.findOne(+id);
+  findOne(@Param('id') id: string,
+    @Req() req: requestAuthUserDto,
+    @Res() res: FastifyReply) {
+    const userId = req.user.sub
+    return this.transactionService.findOne(id, userId);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-    return this.transactionService.update(+id, updateTransactionDto);
+  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto,
+    @Req() req: requestAuthUserDto,
+    @Res() res: FastifyReply) {
+    const userId = req.user.sub
+    return this.transactionService.update(id, userId, updateTransactionDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionService.remove(+id);
+  remove(@Param('id') id: string,
+    @Req() req: requestAuthUserDto,
+    @Res() res: FastifyReply) {
+    const userId = req.user.sub
+    return this.transactionService.remove(id, userId);
   }
 }
