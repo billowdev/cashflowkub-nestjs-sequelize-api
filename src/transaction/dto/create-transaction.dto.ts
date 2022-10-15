27c75c3d526e44b7694a7db3cd224b82bb9d1ff3
Akkarapon Phikulsri <lacakira@gmail.com>
2022-10-15 14:55:58 +0700

    Update create-transaction.dto.ts

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,30 @@
-export class CreateTransactionDto {}
+import { ApiProperty } from "@nestjs/swagger"
+import { IsNotEmpty, IsOptional, IsString } from "class-validator"
+import { TransactionEnum } from "../entities/transaction.entity"
+
+export class CreateTransactionDto {
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	type: TransactionEnum
+
+	@ApiProperty()
+	@IsOptional()
+	@IsString()
+	cashflowinId: string
+
+	@ApiProperty()
+	@IsOptional()
+	@IsString()
+	cashflowoutId: string
+
+	@ApiProperty()
+	@IsOptional()
+	@IsString()
+	transferId: string
+
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	userId: string
+}
