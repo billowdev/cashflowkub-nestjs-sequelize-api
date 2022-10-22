26cdfa161c9fb6185eae7d6d816f9a0212a149ca
Akkarapon Phikulsri <lacakira@gmail.com>
2022-10-22 08:26:18 +0700

    user sign dto document

--- src/auth/dto/sign.dto.ts
+++ src/auth/dto/sign.dto.ts
@@ -1,19 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {  IsNotEmpty, IsObject, IsString } from 'class-validator';
-// import { UserAttributes } from 'src/user/entities/user.entity';
+import { IsNotEmpty, IsObject, IsString } from 'class-validator';
+import { Role } from 'src/user/entities/role.enum';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 export class authDataDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'User data',
+  })
   @IsObject()
   @IsNotEmpty()
-  user: any;
+  user: UserEntity;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The token after generate',
+    example: 'JWT TOKEN'
+  })
   @IsString()
   @IsNotEmpty()
   token: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The user id from session',
+    example: Role.USER
+  })
   @IsString()
   @IsNotEmpty()
   role: string;
@@ -21,12 +30,16 @@ export class authDataDto {
 }
 
 export class SignDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Response message'
+  })
   @IsString()
   @IsNotEmpty()
   message: string;
- 
-  @ApiProperty()
+
+  @ApiProperty({
+    description: 'Data after signin',
+  })
   @IsObject()
   data: authDataDto;
 }
