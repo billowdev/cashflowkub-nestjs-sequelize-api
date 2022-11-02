b086bf8368d25b7898247efd0313e933a40c5136
Akkarapon Phikulsri <lacakira@gmail.com>
2022-11-02 14:47:55 +0700

    update cashflowin.document

--- src/cashflowin/cashflowin.controller.ts
+++ src/cashflowin/cashflowin.controller.ts
@@ -8,6 +8,26 @@ import { BulkCreateCashflowinDto, CreateCashflowinDto } from './dto/create-cashf
 import { UpdateCashflowinDto } from './dto/update-cashflowin.dto';
 import { CashflowinEntity } from './entities/cashflowin.entity';
 import { UserUnauthorizedException } from '../common/swagger-document/unauthorized.document'
+import {
+  ApiCashflowinBulkCreateBadRequestResponse,
+  ApiCashflowinBulkCreateOkResponse,
+  ApiCashflowinGetAllBadRequestResponse,
+  ApiCashflowinGetAllOkResponse,
+  ApiCashflowinGetOneBadRequestResponse,
+  ApiCashflowinGetOneOkResponse,
+  ApiCashflowinGetOneParam,
+  ApiCashflowinBulkCreateBody,
+  ApiCashflowinCreateOkResponse,
+  ApiCashflowinCreateBadRequestResponse,
+  ApiCashflowinCreateBody,
+  ApiCashflowinUpdateParam,
+  ApiCashflowinUpdateBody,
+  ApiCashflowinUpdateOkResponse,
+  ApiCashflowinUpdateBadRequestResponse,
+  ApiCashflowinDeleteParam,
+  ApiCashflowinDeleteOkResponse,
+  ApiCashflowinDeleteBadRequestResponse
+} from './cashflowin.document';
 
 @ApiBearerAuth()
 @ApiTags('Cashflowins')
@@ -18,36 +38,8 @@ export class CashflowinController {
 
 
   @Get()
-  @ApiOkResponse({
-    description: 'get all cashflowins successfuly',
-    schema: {
-      example: {
-        statusCode: 200,
-        message: "get all cashflow in successfuly",
-        data:
-          [
-            {
-              "id": "0184cccf-26fd-47db-a636-d0ebda81fe09",
-              "desc": "my cashflowin 1",
-              "amount": 100,
-              "userId": "41b4f7c2-b221-4a6b-a0e3-d7ec80e0119a",
-              "pocketId": "8407abe9-cbdf-4745-b634-681f42693ee9",
-              "categoryId": "d810173c-f848-4e87-b9f0-d9f172856555",
-              "createdAt": "2022-10-22T10:24:47.400Z",
-              "updatedAt": "2022-10-22T10:24:47.401Z"
-            }
-          ]
-      }
-    }
-  })
-  @ApiBadRequestResponse({
-    description: 'get all cashflowins failed', schema: {
-      example: {
-        statusCode: 400,
-        message: "Unauthorized"
-      }
-    }
-  })
+  @ApiOkResponse(ApiCashflowinGetAllOkResponse)
+  @ApiBadRequestResponse(ApiCashflowinGetAllBadRequestResponse)
   @ApiUnauthorizedResponse(UserUnauthorizedException)
   async findAll(
     @Req() req: RequestWithAuth,
@@ -55,188 +47,50 @@ export class CashflowinController {
   ) {
     const { sub } = req.user
     const data: CashflowinEntity[] = await this.cashflowinService.findAll(sub);
-    res.send({
-      statusCode: res.statusCode,
-      message: "get all cashflow in successfuly",
-      data
-    })
+    res.status(200).send(data)
   }
 
   @Get(':id')
-  @ApiParam({
-    name: 'id',
-    description: 'Enter your cashflowin id that you want to request data',
-    example: '0184cccf-26fd-47db-a636-d0ebda81fe09'
-  })
-  @ApiOkResponse({
-    description: 'get cashflowin successfuly',
-    schema: {
-      example: {
-        statusCode: 200,
-        message: "get all cashflow in successfuly",
-        data:
-        {
-          "id": "0184cccf-26fd-47db-a636-d0ebda81fe09",
-          "desc": "my cashflowin 1",
-          "amount": 100,
-          "userId": "41b4f7c2-b221-4a6b-a0e3-d7ec80e0119a",
-          "pocketId": "8407abe9-cbdf-4745-b634-681f42693ee9",
-          "categoryId": "d810173c-f848-4e87-b9f0-d9f172856555",
-          "createdAt": "2022-10-22T10:24:47.400Z",
-          "updatedAt": "2022-10-22T10:24:47.401Z"
-        }
-
-      }
-    }
-  })
-  @ApiBadRequestResponse({
-    description: 'get cashflowin failed', schema: {
-      example: {
-        statusCode: 400,
-        message: "Unauthorized"
-      }
-    }
-  })
+  @ApiParam(ApiCashflowinGetOneParam)
+  @ApiOkResponse(ApiCashflowinGetOneOkResponse)
+  @ApiBadRequestResponse(ApiCashflowinGetOneBadRequestResponse)
   @ApiUnauthorizedResponse(UserUnauthorizedException)
   async findOne(@Param('id') id: string, @Res() res: FastifyReply) {
     const data: CashflowinEntity = await this.cashflowinService.findOne(id);
-    res.send({
-      statusCode: res.statusCode,
-      message: "get cashflow in by id successfuly",
-      data
-    })
+    res.status(200).send(data)
   }
 
   @Post('bulk')
-  @ApiCreatedResponse({
-    description: 'create cashflowins successfuly',
-    schema: {
-      example: {
-        statusCode: 201,
-        message: "create bulk cashflow in successfuly",
-        data: [
-          {
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            id: '0184cccf-26fd-47db-a636-d0ebda81fe08',
-            desc: "income 1",
-            amount: 100.00,
-            pocketId: '8407abe9-cbdf-4745-b634-681f42693ee9',
-            categoryId: 'd810173c-f848-4e87-b9f0-d9f172856555',
-            userId: '41b4f7c2-b221-4a6b-a0e3-d7ec80e0119a'
-          }]
-      }
-    }
-  })
-  @ApiBadRequestResponse({
-    description: 'cashflowin cannot create. please try again',
-    schema: {
-      example: {
-        statusCode: 400,
-        message: "create cashflowin failed",
-        error: "Bad Request"
-      }
-    }
-  })
-  @ApiBody({
-    description: 'Array body of cashflowin for bulk create',
-    type: CashflowinEntity,
-    isArray: true
-  })
+  @ApiCreatedResponse(ApiCashflowinBulkCreateOkResponse)
+  @ApiBadRequestResponse(ApiCashflowinBulkCreateBadRequestResponse)
+  @ApiBody(ApiCashflowinBulkCreateBody)
   @ApiUnauthorizedResponse(UserUnauthorizedException)
   async bulkCreate(
     @Body() createCashflowinDto: BulkCreateCashflowinDto,
     @Res() res: FastifyReply
   ) {
     const data: CashflowinEntity[] = await this.cashflowinService.bulkCreate(createCashflowinDto);
-
-    res.send({
-      statusCode: res.statusCode,
-      message: "create bulk cashflow in successfuly",
-      data
-    })
+    res.status(201).send(data)
   }
 
   @Post()
-  @ApiCreatedResponse({
-    description: 'create cashflowin successfuly',
-    schema: {
-      example: {
-        statusCode: 201,
-        message: "create cashflow in successfuly",
-        data:
-        {
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          id: '0184cccf-26fd-47db-a636-d0ebda81fe07',
-          desc: "income 2",
-          amount: 100.00,
-          pocketId: '8407abe9-cbdf-4745-b634-681f42693ee9',
-          categoryId: 'd810173c-f848-4e87-b9f0-d9f172856555',
-          userId: '41b4f7c2-b221-4a6b-a0e3-d7ec80e0119a'
-        }
-      }
-    }
-  })
-  @ApiBadRequestResponse({
-    description: 'cashflowin cannot create. please try again',
-    schema: {
-      example: {
-        statusCode: 400,
-        message: "create cashflowin failed",
-        error: "Bad Request"
-      }
-    }
-  })
+  @ApiCreatedResponse(ApiCashflowinCreateOkResponse)
+  @ApiBadRequestResponse(ApiCashflowinCreateBadRequestResponse)
   @ApiUnauthorizedResponse(UserUnauthorizedException)
+  @ApiBody(ApiCashflowinCreateBody)
   async create(
     @Body() createCashflowinDto: CreateCashflowinDto,
     @Res() res: FastifyReply
   ) {
     const data: CashflowinEntity = await this.cashflowinService.create(createCashflowinDto);
-    res.send({
-      statusCode: res.statusCode,
-      message: "create cashflow in successfuly",
-      data
-    })
+    res.status(201).send(data)
   }
 
   @Patch(':id')
-  @ApiParam({
-    name: 'id',
-    description: 'Enter your cashflowin id that you want to update data',
-    example: '0184cccf-26fd-47db-a636-d0ebda81fe09'
-  })
-  @ApiBody({
-    description: 'The body of cashflowin for update',
-    schema: {
-      example: {
-        desc: "ค่ารถ",
-        amount: 100
-      }
-    }
-  })
-  @ApiOkResponse({
-    description: 'update cashflowin successfuly',
-    schema: {
-      example: {
-        statusCode: 200,
-        message: "update cashflowin successfuly",
-        data: [
-          1
-        ]
-      }
-    }
-  })
-  @ApiBadRequestResponse({
-    description: 'update cashflowin failed', schema: {
-      example: {
-        statusCode: 400,
-        message: "update cashflowin failed",
-        error: "Bad Request"
-      }
-    }
-  })
+  @ApiParam(ApiCashflowinUpdateParam)
+  @ApiBody(ApiCashflowinUpdateBody)
+  @ApiOkResponse(ApiCashflowinUpdateOkResponse)
+  @ApiBadRequestResponse(ApiCashflowinUpdateBadRequestResponse)
   @ApiUnauthorizedResponse(UserUnauthorizedException)
   async update(
     @Param('id') id: string,
@@ -250,58 +104,33 @@ export class CashflowinController {
     if (data[0]) {
       res.status(200).send({
         statusCode: res.statusCode,
-        message: "update cashflowin successfuly",
-        data
+        message: "update cashflowin was successfuly",
       })
     } else {
       res.status(400).send({
         statusCode: res.statusCode,
-        message: "update cashflowin failed",
-        data: {}
+        message: "update cashflowin was failed",
       })
     }
 
   }
 
   @Delete(':id')
-  @ApiParam({
-    name: 'id',
-    description: 'Enter your cashflowin id that you want to delete data',
-    example: '0184cccf-26fd-47db-a636-d0ebda81fe09'
-  })
-  @ApiOkResponse({
-    description: 'delete asset successfuly',
-    schema: {
-      example: {
-        statusCode: 200,
-        message: "delete cashflowin successfuly",
-        data: 1
-      }
-    }
-  })
-  @ApiBadRequestResponse({
-    description: 'delete cashflowin failed', schema: {
-      example: {
-        statusCode: 400,
-        message: "delete cashflowin failed",
-        error: "Bad Request"
-      }
-    }
-  })
+  @ApiParam(ApiCashflowinDeleteParam)
+  @ApiOkResponse(ApiCashflowinDeleteOkResponse)
+  @ApiBadRequestResponse(ApiCashflowinDeleteBadRequestResponse)
   @ApiUnauthorizedResponse(UserUnauthorizedException)
   async remove(@Param('id') id: string, @Res() res: FastifyReply, @Req() { user }: RequestWithAuth) {
     const data: number = await this.cashflowinService.remove(id, user.sub);
     if (data) {
       res.status(200).send({
         statusCode: res.statusCode,
-        message: "delete cashflow in by id successfuly",
-        data
+        message: "delete cashflow was successfuly",
       })
     } else {
       res.status(400).send({
         statusCode: res.statusCode,
-        message: "delete cashflow in by id failed",
-        data: {}
+        message: "delete cashflow was failed",
       })
     }
   }
